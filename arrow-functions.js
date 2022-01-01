46fa10e1166b3fcfa3a33b567e7daa0ebf34268b
Olivertrain1221 <olivertrain1996@gmail.com>
2022-01-01 12:27:33 +0000

    Add different ways of using arrow functions

--- arrow-functions.js
+++ arrow-functions.js
@@ -3,12 +3,45 @@
  * command node arrow-functions.js in the terminal
  */
 
+
 // Vanilla JavaScript Function
+
+function addTwoNumbers(a, b) {
+    //code block 
+    return a + b;
+}
+
+let sum = addTwoNumbers(3, 5);
+console.log(sum);
+
 
 // Arrow Function With Parameters
 
+const addTwoNumbers1 = (a, b) => {
+    return a + b;
+}
+
+let sum1 = addTwoNumbers1(3, 3);
+console.log(sum1);
+
+
 // Single Line Arrow Function With Parameters
 
+const addTwoNumbers2 = (a, b) => a + b;
+let sum2 = addTwoNumbers2(2, 4);
+console.log(sum2)
+
 // Implicit Returns
+const saySomething = message => console.log(message);
+saySomething('Hello World');
+
 
 // Returning Multiple Lines
+
+const returnMultiLines = () => (
+    `<p>
+        This is a multiline string!
+    </p>`
+)
+console.log(returnMultiLines());
+
